acee8f2ab7f4c56a910d7a3025a300c04bba2010
bomii1 <202103076@hufs.ac.kr>
2025-01-15 18:12:52 +0900

    feat: oauth 로그인 기능 수정

--- src/components/login/OauthLogin.tsx
+++ src/components/login/OauthLogin.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const OauthLogin = () => {
+  const handleOauthLogin = (provider: string) => {
+    // 백엔드 api 호출
+  };
   return (
     <OauthLoginStyle>
       <div className="oauth-container">
-        <img
-          className="google"
-          src="src/assets/images/google-icon.png"
-          alt="Google Login"
-        />
-        <img src="src/assets/images/kakao-icon.png" alt="Kakao Login" />
-        <img src="src/assets/images/naver-icon.png" alt="Naver Login" />
+        <button onClick={() => handleOauthLogin('google')}>
+          <img
+            className="google"
+            src="src/assets/images/google-icon.png"
+            alt="Google Login"
+          />
+        </button>
+        <button onClick={() => handleOauthLogin('kakao')}>
+          <img src="src/assets/images/kakao-icon.png" alt="Kakao Login" />
+        </button>
+        <button onClick={() => handleOauthLogin('naver')}>
+          <img src="src/assets/images/naver-icon.png" alt="Naver Login" />
+        </button>
       </div>
     </OauthLoginStyle>
   );
@@ -33,9 +42,20 @@ const OauthLoginStyle = styled.div`
   .google {
     padding: 3px;
   }
+
   img {
     width: 35px;
     height: auto;
+  }
+
+  button {
+    background: white;
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.8;
+    }
   }
 `;
 
