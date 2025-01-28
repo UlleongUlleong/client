c458894cd6e22db6cc0169b6323e75a00dd2b30b
bomii1 <202103076@hufs.ac.kr>
2025-01-28 11:05:38 +0900

    feat: 로그인 유지 로직 수정

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -30,18 +30,20 @@ const LoginForm = () => {
       const response = await loginApi(loginContent);
       toast.success(response.message, { icon: <GoCheckCircle /> });
 
-      const now = new Date();
-      const expiresAt = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
-      const formattedDate = `${expiresAt.getFullYear()}-${String(
-        expiresAt.getMonth() + 1,
-      ).padStart(2, '0')}-${String(expiresAt.getDate()).padStart(2, '0')}`;
+      if (isRemembered) {
+        const now = new Date();
+        const expiresAt = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
+        const formattedDate = `${expiresAt.getFullYear()}-${String(
+          expiresAt.getMonth() + 1,
+        ).padStart(2, '0')}-${String(expiresAt.getDate()).padStart(2, '0')}`;
 
-      const loginInfo = {
-        isLoggedIn: true,
-        expiresAt: formattedDate,
-      };
+        const loginInfo = {
+          isLoggedIn: true,
+          expiresAt: formattedDate,
+        };
 
-      localStorage.setItem('loginInfo', JSON.stringify(loginInfo));
+        localStorage.setItem('loginInfo', JSON.stringify(loginInfo));
+      }
 
       setTimeout(() => {
         navigate('/');
