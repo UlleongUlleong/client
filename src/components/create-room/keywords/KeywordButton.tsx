4cf8907cd285e0f309f995997d23dc0e081bbf61
bomii1 <202103076@hufs.ac.kr>
2025-01-30 21:40:10 +0900

    fix: 키워드 버튼 css 에러 수정

--- src/components/create-room/keywords/KeywordButton.tsx
+++ src/components/create-room/keywords/KeywordButton.tsx
@@ -16,14 +16,14 @@ const KeywordButton = ({
   size,
 }: KeywordButtonProps) => {
   return (
-    <KeywordButtonStyle onClick={onClick} isSelected={isSelected} size={size}>
+    <KeywordButtonStyle onClick={onClick} $isSelected={isSelected} size={size}>
       {keyword}
     </KeywordButtonStyle>
   );
 };
 
 interface KeywordButtonStyleProps {
-  isSelected: boolean;
+  $isSelected: boolean;
   size: 'small' | 'large';
 }
 
@@ -32,15 +32,15 @@ const KeywordButtonStyle = styled.button<KeywordButtonStyleProps>`
   border-radius: 30px;
   padding: 5px 15px;
   font-size: ${({ size }) => (size === 'small' ? '0.9rem' : '1rem')};
-  background: ${({ isSelected }) => (isSelected ? '#e8f0fe' : '#f5f4f4')};
-  border-color: ${({ isSelected }) => (isSelected ? '#273ec2' : '#bcbcbc')};
+  background: ${({ $isSelected }) => ($isSelected ? '#e8f0fe' : '#f5f4f4')};
+  border-color: ${({ $isSelected }) => ($isSelected ? '#273ec2' : '#bcbcbc')};
   border-width: 1px;
-  color: ${({ isSelected }) => (isSelected ? '#273ec2' : '#000')};
+  color: ${({ $isSelected }) => ($isSelected ? '#273ec2' : '#000')};
   cursor: pointer;
   transition: all 0.2s ease-in-out;
 
   &:hover {
-    background: ${({ isSelected }) => (isSelected ? '#d7e4fc' : '#e0e0e0')};
+    background: ${({ $isSelected }) => ($isSelected ? '#d7e4fc' : '#e0e0e0')};
   }
 `;
 
