cf1262201b8127b3baf274f030a9227ece77fc2f
bomii1 <202103076@hufs.ac.kr>
2025-02-05 02:21:55 +0900

    feat: 에러 메세지 수정

--- src/components/register/email-verify/EmailVerification.tsx
+++ src/components/register/email-verify/EmailVerification.tsx
@@ -11,7 +11,7 @@ import { GoCheckCircle, GoAlert } from 'react-icons/go';
 
 const EmailVerificationTab = () => {
   const [verificationCode, setVerificationCode] = useState<string>('');
-  const [timeLeft, setTimeLeft] = useState<number>(30);
+  const [timeLeft, setTimeLeft] = useState<number>(600);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const timerRef = useRef<NodeJS.Timeout | null>(null);
@@ -36,7 +36,7 @@ const EmailVerificationTab = () => {
 
   const handleResendCode = async () => {
     try {
-      setTimeLeft(30); // 고치기
+      setTimeLeft(600);
       setErrorMessage(null);
       const response = await requestEmailCode(email);
       toast.success(response.message, { icon: <GoCheckCircle /> });
