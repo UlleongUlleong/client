7b822370049fbca83e4ae8007c36fc8bf4d457c8
Yubeenpark <yoobinpark@gmail.com>
2025-02-06 03:02:06 +0900

    feat(alcohol): 평점 렌더링

--- src/views/pages/Reviews.tsx
+++ src/views/pages/Reviews.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from '../../components/SearchBar';
 import { ReviewsMainContainer } from '../../styles/Reviews';
 import AlcoholEachCategory from './AlcoholEachCategory';
@@ -8,17 +8,35 @@ import styled from 'styled-components';
 import { NoResults } from '../../styles/Alcohol';
 
 import { useCategoryStore } from '../../store/useCategoryStore';
+import { fetchAlcoholsTop10 } from '../../api/alcohol';
+
+import { IAlcohol } from '../../models/alcohol';
 export const LoadingMain = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 0 auto;
 `;
+
+interface AlcoholTop10 {
+  id: number;
+  alcohols: { data: IAlcohol[] };
+}
+
 //리뷰 메인 페이지
 function Reviews() {
-  useEffect(() => {});
+  const [top10, setTop10] = useState<AlcoholTop10>(null);
+
+  useEffect(() => {
+    const fetchAlcohols = async (top: number) => {
+      const top10Data = await fetchAlcoholsTop10(top);
+      setTop10(top10Data);
+    };
+    fetchAlcohols(10);
+  }, []);
+
   const category = useCategoryStore((state) => state.alcoholCategories);
-  // const top10 = await fetchAlcoholsTop10(10);
+
   // const alcoholCategories = [{ id: 0, name: '평점 TOP 10' }, ...category];
   const { categoriesData, isLoading, isError } = useAlcoholsByCategory();
   if (isLoading)
@@ -31,7 +49,12 @@ function Reviews() {
   return (
     <ReviewsMainContainer>
       <SearchBar isMoodCategories={false} />
-
+      <AlcoholEachCategory
+        key={0}
+        categoryId={0}
+        categoryName={'평점 TOP 10'}
+        alcoholsData={top10[0]?.alcohols.data}
+      />
       {category.map((category) => (
         <AlcoholEachCategory
           key={category.id}
