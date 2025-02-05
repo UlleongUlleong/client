95685d98645e10f927f2a5909b6480775e38b025
Yubeenpark <yoobinpark@gmail.com>
2025-02-05 17:33:39 +0900

    design(font): 폰트 전역 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import { worker } from './mocks/browser.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
 function renderApp() {
   const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
